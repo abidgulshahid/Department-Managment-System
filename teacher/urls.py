c2aaaf5cd1ce66f894abd3911c15ab126b48f8d9
Abid Gul Shahid <silencemind55@gmail.com>
2021-06-08 18:29:10 +0500

    .

--- teacher/urls.py
+++ teacher/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('<slug:t>/', views.view_assignments_page, name='view_assignments_page'),
 
-    path('<slug:assi>/',views.add_assigment,name='add_assigment'),
+    path('add/<slug:teacher_id>/',views.add_assigment,name='add_assigment'),
 
     path('marks/<slug:stud>/<slug:teach>/', views.teacher_view_marks, name='teacher_view_marks'),
     path('marks/<slug:stud>/<slug:teach>/take', views.take_marks, name='take_marks'),
